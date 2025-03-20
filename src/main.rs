f70119383ce477087059605a1ef6ce1d00c50a17
Ujjwal KC <kcujwal98@gmail.com>
2025-03-20 21:39:17 +0545

    SDL Window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main(){
-    println!("Hello");
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
+pub fn main(){
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("First Window", 600, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter(){
+            match event{
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::ESCAPE), ..} => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+    }
+
 }
