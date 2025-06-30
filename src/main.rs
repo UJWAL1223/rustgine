7f599fa46491c4dbb97b1576d0df8a9fa6c84ad7
Ujjwal KC <kcujwal98@gmail.com>
2025-06-30 17:27:56 +0545

    Window

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-extern crate sdl2;
-
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
 
-pub fn main(){
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+pub fn main() -> Result<(), String>{
 
-    let window = video_subsystem.window("First Window", 600, 600)
+    let sdl = sdl2::init()?;
+    let video_subsystem = sdl.video()?;
+
+    let window = video_subsystem.window("Rustgine", 1920, 1080)
         .position_centered()
+        .opengl()
         .build()
-        .unwrap();
+        .map_err(|e| e.to_string());
 
-    let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+        let mut event_pump = sdl.event_pump()?;
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    'running: loop {
-        for event in event_pump.poll_iter(){
-            match event{
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::ESCAPE), ..} => {
-                    break 'running
-                },
-                _ => {}
+        'running: loop {
+            for event in event_pump.poll_iter(){
+                match event {
+                    Event::Quit { .. } | Event::KeyDown{ keycode: Some(Keycode::Escape), .. } => {break 'running}
+                    _ => {}
+                }
             }
         }
-    }
 
+    Ok(())
 }
